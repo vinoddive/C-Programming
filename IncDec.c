e5e2b109ffdc2ec5445c4077528ec482696a5ed1
vinoddive <divevinod8767@gmail.com>
2026-06-30 10:31:16 +0530

    Increment and Decrement Operators in c language

---
+++ IncDec.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    
+    int a=10, b=100;
+    float c=11.5, d=100.5;
+
+    printf(" ++a = %d \n", a);
+    printf(" --b = %d \n", b);
+    printf(" ++c = %d \n", c);
+    printf(" --d = %d \n", d);
+}
